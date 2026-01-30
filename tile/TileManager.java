032852e3a8cc166e5c37d29a9ee1205446b431c9
Mgika32 <mickaelguemin2@gmail.com>
2026-01-30 22:46:36 +0100

    modification du GamePanel et du tileM pour coller a la map

--- tile/TileManager.java
+++ tile/TileManager.java
@@ -19,7 +19,7 @@ public class TileManager {
     public TileManager(GamePanel gp) {
         this.gp = gp;
 
-        tile = new Tile[100];
+        tile = new Tile[20000];
         mapTileNum = new int[gp.maxWorldCol][gp.maxWorldRow];
 
         getTileImage();
@@ -41,82 +41,9 @@ public class TileManager {
 
     public void getTileImage() {
         // GRASS
-        setup(10, "grass00", false);
-        setup(11, "grass01", false);
-
-        // WATER
-        setup(12, "water00", true);
-        setup(13, "water01", true);
-        setup(14, "water02", true);
-        setup(15, "water03", true);
-        setup(16, "water04", true);
-        setup(17, "water05", true);
-        setup(18, "water06", true);
-        setup(19, "water07", true);
-        setup(20, "water08", true);
-        setup(21, "water09", true);
-        setup(22, "water10", true);
-        setup(23, "water11", true);
-        setup(24, "water12", true);
-        setup(25, "water13", true);
-
-        // ROAD
-        setup(26, "road00", false);
-        setup(27, "road01", false);
-        setup(28, "road02", false);
-        setup(29, "road03", false);
-        setup(30, "road04", false);
-        setup(31, "road05", false);
-        setup(32, "road06", false);
-        setup(33, "road07", false);
-        setup(34, "road08", false);
-        setup(35, "road09", false);
-        setup(36, "road10", false);
-        setup(37, "road11", false);
-        setup(38, "road12", false);
-
-        // DIVERS
-        setup(39, "floor01", false);
-        setup(40, "wall", true);
-
-        // TREE
-        setup(41, "tree_1", true);
-        setup(42, "tree_2", true);
-        setup(43, "tree_3", true);
-        setup(44, "tree_4", true);
-
-        // HOUSE
-
-        setup(50, "house_1", true);
-        setup(51, "house_2", true);
-        setup(52, "house_3", true);
-        setup(53, "house_4", true);
-        setup(54, "house_5", true);
-        setup(55, "house_6", true);
-        setup(56, "house_7", true);
-        setup(57, "house_8", true);
-        setup(58, "house_9", true);
-        setup(59, "house_10", true);
-        setup(60, "house_11", true);
-        setup(61, "house_12", true);
-        setup(62, "house_13", true);
-        setup(63, "house_14", true);
-        setup(64, "house_15", true);
-        setup(65, "house_16", true);
-        setup(66, "house_17", true);
-        setup(67, "house_18", true);
-        setup(68, "house_19", true);
-        setup(69, "house_20", true);
-        setup(70, "house_21", true);
-        setup(71, "house_22", true);
-        setup(72, "house_23", true);
-        setup(73, "house_24", true);
-        setup(74, "house_25", true);
-        setup(75, "house_26", false);
-        setup(76, "house_27", false);
-        setup(77, "house_28", false);
-        setup(78, "house_29", false);
-        setup(79, "house_30", false);
+        setup(00, "grass", false);
+        setup(10, "path", false);
+        setup(30, "wall", true);
 
     }
 
@@ -159,14 +86,12 @@ public class TileManager {
             int screenX = worldX - gp.player.worldX + gp.player.screenX;
             int screenY = worldY - gp.player.worldY + gp.player.screenY;
 
-            // Rendu optimisé (Culling)
             if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
                 worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
                 worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
                 worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
 
                 if (tile[tileNum] != null) {
-                    // On ne met plus de gp.tileSize ici, car l'image est DEJA à la bonne taille !
                     g2.drawImage(tile[tileNum].image, screenX, screenY, null);
                 }
             }
