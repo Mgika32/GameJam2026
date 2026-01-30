0ab35527ba4234b3596c424699626c296a3aba0a
Graphikito <wtdp8nftxp@privaterelay.appleid.com>
2026-01-30 23:21:56 +0100

    Modif player

--- Entity/Player.java
+++ Entity/Player.java
@@ -1,6 +1,7 @@
 package Entity;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import main.GamePanel;
@@ -17,6 +18,7 @@ public class Player extends entity{
         this.keyH = keyH;
 
         setDefaultValues();
+        getPlayerImage();
 
 
     } 
@@ -25,6 +27,7 @@ public class Player extends entity{
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
     }
     public void getPlayerImage() {
         try {
@@ -43,25 +46,74 @@ public class Player extends entity{
     }
     public void update() {
 
-        if (keyH.upPressed == true) {
-            y -= speed;
-        }
-        if (keyH.downPressed == true) {
-            y += speed;
-        }
-        if (keyH.leftPressed == true) {
-            x -= speed;
-        }
-        if (keyH.rightPressed == true) {
-            x += speed;
-        }
+        if(keyH.upPressed == true || keyH.downPressed == true || keyH.leftPressed == true || keyH.rightPressed == true) {
+            if (keyH.upPressed == true) {
+                direction = "up";
+                y -= speed;
+            }
+            if (keyH.downPressed == true) {
+                direction = "down";
+                y += speed;
+            }
+            if (keyH.leftPressed == true) {
+                direction = "left";
+                x -= speed;
+            }
+            if (keyH.rightPressed == true) {
+                direction = "right";
+                x += speed;
+            }
 
+            spriteCounter++;
+            if(spriteCounter > 14) {
+                if(spriteNum == 1) {
+                    spriteNum = 2;
+                }
+                else if(spriteNum == 2) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
+        }
     }
     public void draw(Graphics2D g2) {
-
-        g2.setColor(Color.white);
-
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
+        
+        switch(direction) {
+            case "up":
+                if(spriteNum == 1) {
+                    image = up1;
+                }
+                if (spriteNum == 2) {
+                    image = up2;
+                }
+                break;
+            case "down":
+                if(spriteNum == 1) {
+                    image = down1;
+                }
+                if(spriteNum == 2) {
+                    image = down2;
+                }
+                break;
+            case "left":
+                if(spriteNum == 1) {
+                    image = left1;
+                }
+                if(spriteNum == 2) {
+                    image = left2;
+                }
+                break;
+            case "right":
+                if(spriteNum == 1) {
+                    image = right1;
+                }
+                if(spriteNum == 2) {
+                    image = right2;
+                }
+                break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 
 }
